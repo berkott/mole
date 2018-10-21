d250c99c9e8bdb7ad28c4e799d1069d93f35ab53
berkott <berkott@gmail.com>
2018-10-21 13:32:13 -0400

    fixing motors

--- main.py
+++ main.py
@@ -85,4 +85,14 @@ def pickAction(command):
     else:
         playSounds("confused")
 
+def set_servo_pulse(channel, pulse):
+    pulse_length = 1000000    # 1,000,000 us per second
+    pulse_length //= 60       # 60 Hz
+    print('{0}us per period'.format(pulse_length))
+    pulse_length //= 4096     # 12 bits of resolution
+    print('{0}us per bit'.format(pulse_length))
+    pulse *= 1000
+    pulse //= pulse_length
+    pwm.set_pwm(channel, 0, pulse)
+
 main()
